09b978416f335b0d7772d16f7d182034f07c56b9
Pete Michaud <michaudp@gmail.com>
2013-09-17 20:39:46 -0500

    fixing blog image plugin

--- _plugins/blog_image.rb
+++ _plugins/blog_image.rb
@@ -8,7 +8,7 @@ module Jekyll
 
     def render(context)
       format("<div class='blog_img'><img src='%s%s' alt='%s' title='%s' /><div class='caption'>%s</div></div>",
-             context.registers[:site].config['blog_image_path'],
+             (context.registers[:site].config['blog_image_path'] || '/files/blog/images/'),
              @image_file_name,
              @caption,
              @caption,
